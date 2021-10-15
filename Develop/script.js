4153e636d4dfbc1afb9f14bba219470bb39287f2
CFM <cpierrelouis37@gmail.com>
2021-10-15 07:43:07 -0500

    Mission accomplished

--- Develop/script.js
+++ Develop/script.js
@@ -2,88 +2,190 @@
 var generateBtn = document.querySelector("#generate");
 //variable to save prompt response
 
-// Define lower case8, upper case, special characters
-const specialCase = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ";", "<", "=", ">", "?", "@", "[", "_", "`", "{", "|", "}", "~", "]", "/", "^"];
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-console.log(specialCase)
-console.log(specialCase.length)
-console.log(alphabet)
-
-
 //Define generate password function 
 function generatePassword() {
   // console.log( "You clicked the button")
   var howManyChar = prompt("How many characters would you like?")
   if ((howManyChar > 128) || (howManyChar < 8)) {
-    alert("Oops! Refresh the page ans try again");
+    alert("Oops! Refresh the page and try again");
+    //return function    //loop the erroroneous people to the top of this variable
   }
-  //loop the erroroneous people to the top of this variable
-
-
   else {
-   
+
     confirm("You would like " + howManyChar + " characters.")
-    var Symbols = confirm("Would you like special characters?")
-    if (Symbols === true) {
-      var symbolsSet = "!#$%&'()*+,-./:;<=>?@[^_`{|}~/]"
-      // console.log(symbolsSet) confirmed that the variable symbolset was successfully made
-   
-    } else {
-      symbolsSet = ""
-    }
-    
-    var Num = confirm("Would you like to include numbers?")
-    if (Num === true) {
-      var numSet = "0123456789"
-      // console.log(symbolsSet) confirmed that the variable symbolset was successfully made
-    } else {
-      numSet = ""
-    }
+  }
+  //Password variables
+  var Symbols = confirm("Would you like special characters?")
+  let strS = "!#$%&'()*+,-./:;<=>?@[^_`{|}~/]"
+  if (Symbols === false) {
+    let nostrS = ""
+  }
 
-    var upper = confirm("Would you like to include uppercase letters?")
-    if (upper === true) {
-      var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      // console.log(symbolsSet) confirmed that the variable symbolset was successfully made
-    } else {
-      upperSet = ""
+  var Num = confirm("Would you like to include numbers?")
+  let strN = "0123456789"
+  if (Num === false) {
+    nostrN = ""
+  }
+  let ySyN = strS.concat(strN);
+
+  if ((Symbols == true) && (Num == true)) {
+    // console.log(ySyN) concant worked
+  } var upper = confirm("Would you like to include uppercase letters?")
+  let strU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  if (upper === false) {
+    nostrU = ""
+  }
+  var lower = confirm("Would you like to include lowercase letters?")
+  let strL = "abcdefghijklmnopqrstuvwxyz"
+
+  if (lower === false) {
+    nostrL = ""
+  }
+  // Selections
+  // All are selected, 
+  if ((Symbols == true) && (Num == true) && (upper == true) && (lower == true)) {
+    let ySyNyUyL = strS.concat(strN, strU, strL);
+    var returnValue = "";
+    var length = parseInt(howManyChar, 10);
+    for (var i = 0; i < (length); ++i) {
+      returnValue += ySyNyUyL.charAt(Math.floor(Math.random() * ySyNyUyL.length));
+    } return returnValue;
+  }
+  else if ((Symbols == true) && (Num == true) && (upper == true) && (lower == false)) {
+    let ySyNyUnL = strS.concat(strN, strU, nostrL);
+    var returnValue = "";
+    var length = parseInt(howManyChar, 10);
+    for (var i = 0; i < (length); ++i) {
+      returnValue += ySyNyUnL.charAt(Math.floor(Math.random() * ySyNyUnL.length));
     }
-    var lower = confirm("Would you like to include lowercase letters?")
-    if (lower === true) {
-      var lowerSet = "abcdefghijklmnopqrstuvwxyz"
-      // console.log(symbolsSet) confirmed that the variable symbolset was successfully made
-    } else {
-      lowerSet = ""
-    }
-    returnValue = "";
-    charset = "ASDFGFDS^%$#$@T%^$Y%$"
-for (var i = 0, n = howManyChar; i < 0 ; ++i) {
-returnValue += charset.charAt(Math.floor(Math.random() * n));
+    return returnValue;
+  }
+  else if ((Symbols == true) && (Num == true) && (upper == false) && (lower == false)){
+    let y1 = strS.concat(strN, nostrU, nostrL);
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
 }
-return returnValue;
+ else if((Symbols == true) && (Num == false) && (upper==false) && (lower==false)){
+  let y1 = strS.concat(nostrN, nostrU, nostrL);
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+}
+else if((Symbols == false) && (Num == true) && (upper==true) && (lower==true)){
+  let y1 = strN.concat(strU, strL);
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+}
+ else if((Symbols == false) && (Num == true) && (upper==false) && (lower==false)){
+  let y1 = strN;
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+}
+ else if((Symbols == false) && (Num == true) && (upper==false) && (lower==true)){
+  let y1 = strN.concat(strL);
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+ }
+ else if((Symbols == false) && (Num == false) && (upper==true) && (lower==false)){
+  let y1 = strU;
+  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+ } 
+ else if((Symbols == false) && (Num == true) && (upper==true) && (lower==false)){
+  let y1 = strU.concat(strN);  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+}
+  else if((Symbols == false) && (Num == true) && (upper==true) && (lower==true)){
+  let y1 = strU.concat(strN, strL);  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+  return returnValue;
+ } else if((Symbols == false) && (Num == false) && (upper==false) && (lower==true)){
+  let y1 = strL;  var returnValue = "";
+  var length = parseInt(howManyChar, 10);
+  for (var i = 0; i < (length); ++i) {
+    returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+  }
+ }
+ else if((Symbols == false) && (Num == false) && (upper==true) && (lower==true)){
+
+    let y1 = strL.concat(strU);  var returnValue = "";
+    var length = parseInt(howManyChar, 10);
+    for (var i = 0; i < (length); ++i) {
+      returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
     }
-}
+   }
+  else if((Symbols == false) && (Num == true) && (upper==true) && (lower==true)){
+    let y1 = strL.concat(strN, strU);  var returnValue = "";
+    var length = parseInt(howManyChar, 10);
+    for (var i = 0; i < (length); ++i) {
+      returnValue += y1.charAt(Math.floor(Math.random() * y1.length));
+    }
+   }
+    else ((Symbols == false) && (Num == false) && (upper==false) && (lower==false))
+      alert("You must select symbols, numbers, upper case characters, or lower case characters to generate a passcode");
+        
+  }
+  // var length = howManyChar.length;
+  // var returnValue = "";
+  // var length = parseInt(howManyChar, 10);
+  // for (var i = 0; i < (length); ++i) {
+  //   returnValue += ySyNyUyL.charAt(Math.floor(Math.random() * ySyNyUyL.length));
+  // }
+  // return returnValue;
 
-//confirms that the button clicks works. The button now produces undefined instead of nothing.
-//1. prompt the user for the password criteria 
-//2.validate inputs
-//  if they type in something wrong, write an error alert.
-//3. Generate password based on criteria 
-//4. Display the generate password on the page
+  // }
+  // }
+  // //confirms that the button clicks works. The button now produces undefined instead of nothing.
+  // //1. prompt the user for the password criteria 
+  // //2.validate inputs
+  // //  if they type in something wrong, write an error alert.
+  // //3. Generate password based on criteria 
+  // //4. Display the generate password on the page
 
-// return "Generated password goes here" //confirms we want the button to return something and I can confirm the structure.
-// }
+  // // return "Generated password goes here" //confirms we want the button to return something and I can confirm the structure.
+  // // }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+    return password
+  }
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
 /*WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
